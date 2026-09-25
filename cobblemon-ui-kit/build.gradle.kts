b8d9113fe49537ec1e63705668fc35591511a374
PPAK_JU <153336193+taku7664@users.noreply.github.com>
2026-09-26 00:03:20 +0900

    fix(ui): use Cobblemon Mojmap artifacts

--- cobblemon-ui-kit/build.gradle.kts
+++ cobblemon-ui-kit/build.gradle.kts
@@ -14,7 +14,10 @@ dependencies {
     modImplementation("net.fabricmc:fabric-loader:${property("fabric_loader_version")}")
     modImplementation("net.fabricmc.fabric-api:fabric-api:${property("fabric_api_version")}")
     modImplementation("net.fabricmc:fabric-language-kotlin:${property("fabric_kotlin_version")}")
-    modImplementation("maven.modrinth:cobblemon:${property("cobblemon_version_id")}")
+    modCompileOnly("com.cobblemon:mod:${property("cobblemon_maven_version")}") {
+        isTransitive = false
+    }
+    modImplementation("com.cobblemon:fabric:${property("cobblemon_maven_version")}")
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
     testImplementation("com.google.code.gson:gson:2.11.0")
